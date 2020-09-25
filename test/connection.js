910d8a8bc1638e5341784ea416277c06af774220
TJ <tahmeenaowais@yahoo.com>
2020-09-24 21:10:44 -0400

    change name

--- test/connection.js
+++ test/connection.js
@@ -3,7 +3,8 @@ var mysql = require("mysql");
 var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
+} else 
+{
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
